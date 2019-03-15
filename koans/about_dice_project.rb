56903c576dc6b5a368be714348e53d48a39edbcd
Ryan Schranz <ryan.schranz@money.co.uk>
2019-03-15 15:01:55 +0000

    Roll a random amount of dice and access their values

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def roll(numberDice)
+    @value = []
+
+    numberDice.times do |n|
+      @value.push(rand(1..6))
+    end
+  end
+
+  def values
+    @value
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
